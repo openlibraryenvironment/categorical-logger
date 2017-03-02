2b9212f06350263bb4afea91f0f26e7f67515875
Mike Taylor <mike@indexdata.com>
2017-03-02 15:19:10 +0000

    New file, exercise-logging.js

---
+++ exercise-logging.js
@@ -0,0 +1,14 @@
+const Logger = require('./stripes-logger.js');
+
+const l = new Logger('redux,action');
+
+l.setLevel('redux,path,cql,error,action');
+l.setPrefix('stripes');
+l.setTimestamp(true);
+
+const path = 'item-storage/items?query=materialType%3D*%20sortby%20title';
+const query = 'water';
+
+l.log('path', `substitutePath generated ${path}`);
+l.log('action', 'user searched for', query); // will emit no output
+l.log('cql', () => [query, path]); // test ability to pass a function
